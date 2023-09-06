2e151fe270c8ea83dba2ee3550eaf310300cafac
rfuzzo <r.fuzzo@gmail.com>
2023-09-06 19:08:56 +0200

    disable wasm

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,3 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
     )
 }
-
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                "the_canvas_id", // hardcode it
-                web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
-}
